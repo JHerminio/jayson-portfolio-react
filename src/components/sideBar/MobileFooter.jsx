7d763786731f3a6fc71b296ae2deaabc2734ac00
Jayson Herminio <jaysonberherminio@gmail.com>
2026-09-12 12:38:44 +0800

    new

--- src/components/sideBar/MobileFooter.jsx
+++ src/components/sideBar/MobileFooter.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function MobileFooter() {
-  const currentYear = new Date().getFullYear();
+  const [currentYear, setCurrentYear] = useState(null);
+
+  useEffect(() => {
+    setCurrentYear(new Date().getFullYear());
+  }, []);
 
   return (
     <footer className="mobile-bottom-footer">
       <i className="ph-fill ph-seal-check"></i>
-      <p>© {currentYear} sssJayson Herminio. All rights reserved.</p>
+      <p>
+        ©{" "}
+        <span suppressHydrationWarning>
+          {currentYear || new Date().getFullYear()}
+        </span>{" "}
+        Jayson Herminio. All rights reserved.
+      </p>
     </footer>
   );
 }
